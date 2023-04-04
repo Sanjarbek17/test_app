5b2696d2c7e820e8ce7de8875d4a745edc57803f
sanjarbek <sanjarsaidov173@gmail.com>
2023-04-04 14:42:56 +0500

    some changes

--- lib/style/main_style.dart
+++ lib/style/main_style.dart
@@ -3,5 +3,29 @@ import 'package:flutter/material.dart';
 import 'constant.dart';
 
 ThemeData theme = ThemeData(
-  primaryColor: shark,
+  colorScheme: ColorScheme.light(
+    primary: shark,
+    secondary: bilobaFlower,
+    onSecondary: Colors.white,
+  ),
+  textTheme: TextTheme(
+    titleLarge: TextStyle(
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w700,
+      fontSize: 18,
+      color: black,
+    ),
+    titleMedium: TextStyle(
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400,
+      fontSize: 14,
+      color: silverSand,
+    ),
+    titleSmall: TextStyle(
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400,
+      fontSize: 12,
+      color: grey,
+    ),
+  ),
 );
