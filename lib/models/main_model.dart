b01a4ba3fa5672e933ddc3540e7f629bba56cf29
sanjarbek <sanjarsaidov173@gmail.com>
2023-04-04 12:47:31 +0500

    product model created

---
+++ lib/models/main_model.dart
@@ -0,0 +1,19 @@
+class Product {
+  final int id;
+  final String name;
+  final String code;
+  final double distance;
+  final String address;
+  final int time;
+  final String storeType;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.code,
+    required this.distance,
+    required this.address,
+    required this.time,
+    required this.storeType,
+  });
+}
