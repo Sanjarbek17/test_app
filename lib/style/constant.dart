29b185cf67e34e97f93143547ca39643c37ccdbb
sanjarbek <sanjarsaidov173@gmail.com>
2023-04-04 12:47:19 +0500

    colors added

---
+++ lib/style/constant.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// product name color
+Color shark = const Color.fromARGB(255, 48, 50, 55);
+// background color
+Color concrete = const Color.fromARGB(255, 242, 242, 242);
+// product address text color
+Color silverSand = const Color.fromARGB(255, 190, 191, 191);
+// selected navigations color
+Color osloGray = const Color.fromARGB(255, 147, 149, 149);
+// product address text color 2
+Color cornflowerBlue = const Color.fromARGB(255, 116, 148, 244);
+// buy type icon background color
+Color bilobaFlower = const Color.fromARGB(255, 228, 150, 238);
+// buy type icon border color
+Color lavender = const Color.fromARGB(255, 220, 116, 236);
+// work type icon background color
+Color mossGreen = const Color.fromARGB(255, 180, 220, 172);
+// work type icon border color
+Color deYork = const Color.fromARGB(255, 124, 188, 116);
---
+++ lib/style/main_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'constant.dart';
+
+ThemeData theme = ThemeData(
+  primaryColor: shark,
+);
